628f77254d7d372d34d08d284229ac802cc9dc9f
onat mufasa <mufasaonat@gmail.com>
2026-06-20 20:14:05 +0700

    ...

--- javascript/script.js
+++ javascript/script.js
@@ -1,73 +1,73 @@
 (function() {
     
     const studentSites = {
-        'M3101': 'https://johndoe.vercel.app',
-        'M3102': 'https://sarahsmith.github.io',
-        'M3103': 'https://alexchen.netlify.app',
-        'M3104': 'https://emilyportfolio.com',
-        'M3105': 'https://michael-dev.herokuapp.com',
-        'M3106': 'https://jessicacode.io',
-        'M3107': 'https://davidkim.tech',
-        'M3108': 'https://amandawrites.dev',
-        'M3109': 'https://briancodes.xyz',
-        'M3110': 'https://lauragreen.design',
-        'M3111': 'https://kevinm.vercel.app',
-        'M3112': 'https://rachelchen.dev',
-        'M3113': 'https://jasonliu.works',
-        'M3114': 'https://ninapatel.com',
-        'M3115': 'https://ericwang.me',
-        'M3116': 'https://michelledev.xyz',
-        'M3117': 'https://justinbrown.dev',
-        'M3118': 'https://ashleynguyen.design',
-        'M3119': 'https://mattthomas.io',
-        'M3120': 'https://oliviapark.art',
-        'M3121': 'https://ryangarcia.dev',
-        'M3122': 'https://emmarodriguez.com',
-        'M3123': 'https://noahmartin.tech',
-        'M3124': 'https://chloejackson.xyz',
-        'M3125': 'https://liamwilson.dev',
-        'M3126': 'https://zoedavis.design',
-        'M3127': 'https://elijahbrown.io',
-        'M3128': 'https://millermiller.art',
-        'M3129': 'https://gracekim.dev',
-        'M3130': 'https://oliverlee.tech',
-        'M3201': 'https://sophiawalker.com',
-        'M3202': 'https://jacksonmoore.dev',
-        'M3203': 'https://averytaylor.xyz',
-        'M3204': 'https://averytaylor.xyz',
-        'M3205': 'https://averytaylor.xyz',
-        'M3206': 'https://averytaylor.xyz',
-        'M3207': 'https://averytaylor.xyz',
-        'M3208': 'https://averytaylor.xyz',
-        'M3209': 'https://averytaylor.xyz',
-        'M3210': 'https://averytaylor.xyz',
-        'M3211': 'https://averytaylor.xyz',
-        'M3212': 'https://averytaylor.xyz',
-        'M3213': 'https://averytaylor.xyz',
-        'M3214': 'https://averytaylor.xyz',
-        'M3215': 'https://averytaylor.xyz',
-        'M3216': 'https://averytaylor.xyz',
-        'M3217': 'https://averytaylor.xyz',
-        'M3218': 'https://averytaylor.xyz',
-        'M3219': 'https://averytaylor.xyz',
-        'M3220': 'https://averytaylor.xyz',
-        'M3221': 'https://averytaylor.xyz',
-        'M3222': 'https://averytaylor.xyz',
-        'M3223': 'https://averytaylor.xyz',
-        'M3224': 'https://averytaylor.xyz',
-        'M3225': 'https://averytaylor.xyz',
-        'M3226': 'https://averytaylor.xyz',
-        'M3227': 'https://averytaylor.xyz',
-        'M3228': 'https://averytaylor.xyz',
-        'M3229': 'https://averytaylor.xyz',
-        'M3230': 'https://averytaylor.xyz'
+        'M3101': 'https://johndoe.vercel.app/portfolio/',
+        'M3102': 'https://sarahsmith.github.io/portfolio/',
+        'M3103': 'https://alexchen.netlify.app/portfolio/',
+        'M3104': 'https://emilyportfolio.com/portfolio/',
+        'M3105': 'https://michael-dev.herokuapp.com/portfolio/',
+        'M3106': 'https://jessicacode.io/portfolio/',
+        'M3107': 'https://davidkim.tech/portfolio/',
+        'M3108': 'https://amandawrites.dev/portfolio/',
+        'M3109': 'https://briancodes.xyz/portfolio/',
+        'M3110': 'https://lauragreen.design/portfolio/',
+        'M3111': 'https://kevinm.vercel.app/portfolio/',
+        'M3112': 'https://rachelchen.dev/portfolio/',
+        'M3113': 'https://jasonliu.works/portfolio/',
+        'M3114': 'https://ninapatel.com/portfolio/',
+        'M3115': 'https://ericwang.me/portfolio/',
+        'M3116': 'https://michelledev.xyz/portfolio/',
+        'M3117': 'https://justinbrown.dev/portfolio/',
+        'M3118': 'https://ashleynguyen.design/portfolio/',
+        'M3119': 'https://mattthomas.io/portfolio/',
+        'M3120': 'https://oliviapark.art/portfolio/',
+        'M3121': 'https://ryangarcia.dev/portfolio/',
+        'M3122': 'https://emmarodriguez.com/portfolio/',
+        'M3123': 'https://noahmartin.tech/portfolio/',
+        'M3124': 'https://chloejackson.xyz/portfolio/',
+        'M3125': 'https://liamwilson.dev/portfolio/',
+        'M3126': 'https://zoedavis.design/portfolio/',
+        'M3127': 'https://elijahbrown.io/portfolio/',
+        'M3128': 'https://millermiller.art/portfolio/',
+        'M3129': 'https://gracekim.dev/portfolio/',
+        'M3130': 'https://oliverlee.tech/portfolio/',
+        'M3201': 'https://sophiawalker.com/portfolio/',
+        'M3202': 'https://jacksonmoore.dev/portfolio/',
+        'M3203': 'https://averytaylor.xyz/portfolio/',
+        'M3204': 'https://averytaylor.xyz/portfolio/',
+        'M3205': 'https://averytaylor.xyz/portfolio/',
+        'M3206': 'https://averytaylor.xyz/portfolio/',
+        'M3207': 'https://averytaylor.xyz/portfolio/',
+        'M3208': 'https://averytaylor.xyz/portfolio/',
+        'M3209': 'https://averytaylor.xyz/portfolio/',
+        'M3210': 'https://averytaylor.xyz/portfolio/',
+        'M3211': 'https://averytaylor.xyz/portfolio/',
+        'M3212': 'https://averytaylor.xyz/portfolio/',
+        'M3213': 'https://averytaylor.xyz/portfolio/',
+        'M3214': 'https://averytaylor.xyz/portfolio/',
+        'M3215': 'https://averytaylor.xyz/portfolio/',
+        'M3216': 'https://averytaylor.xyz/portfolio/',
+        'M3217': 'https://averytaylor.xyz/portfolio/',
+        'M3218': 'https://averytaylor.xyz/portfolio/',
+        'M3219': 'https://averytaylor.xyz/portfolio/',
+        'M3220': 'https://averytaylor.xyz/portfolio/',
+        'M3221': 'https://averytaylor.xyz/portfolio/',
+        'M3222': 'https://averytaylor.xyz/portfolio/',
+        'M3223': 'https://averytaylor.xyz/portfolio/',
+        'M3224': 'https://averytaylor.xyz/portfolio/',
+        'M3225': 'https://averytaylor.xyz/portfolio/',
+        'M3226': 'https://averytaylor.xyz/portfolio/',
+        'M3227': 'https://averytaylor.xyz/portfolio/',
+        'M3228': 'https://averytaylor.xyz/portfolio/',
+        'M3229': 'https://averytaylor.xyz/portfolio/',
+        'M3230': 'https://averytaylor.xy/portfolio/z'
     };
 
     
     const colorPalette = [
         '#FF6B6B', '#FF8E53', '#FECA57', '#48DBFB', '#0ABDE3', '#10AC84',
-        '#EE5A24', '#5F27CD', '#341F97', '#FF9FF3', '#54A0FF', '#5F6DEC',
-        '#FF6348', '#7BED9F', '#70A1FF', '#FF4757', '#2ED573', '#1E90FF',
+        '#EE5A24', '#5F27CD', '#220d7c', '#FF9FF3', '#54A0FF', '#5F6DEC',
+        '#FF6348', '#1cb54c', '#70A1FF', '#FF4757', '#2ED573', '#1E90FF',
         '#FF6B81', '#7F8FA6', '#F368E0', '#00D2D3', '#54A0FF', '#5F27CD',
         '#FF9F43', '#10AC84', '#EE5A24', '#0ABDE3', '#341F97', '#FF4757',
         '#2ED573', '#1E90FF', '#FF6B81', '#7F8FA6', '#F368E0', '#00D2D3',
@@ -99,16 +99,13 @@
             card.className = 'student-card';
             card.style.animationDelay = (0.02 + index * 0.03) + 's';
             card.dataset.studentId = id;
-
             
             const colorIndex = index % colorPalette.length;
-            const cardColor = colorPalette[colorIndex];
-            
+            const cardColor = colorPalette[colorIndex];            
             
             card.style.background = `linear-gradient(135deg, ${cardColor}22 0%, ${cardColor}44 100%)`;
             card.style.borderColor = cardColor;
-            card.style.borderWidth = '2px';
-            
+            card.style.borderWidth = '2px';            
             
             card.dataset.color = cardColor;
 
@@ -117,10 +114,8 @@
 
             card.innerHTML = `
                 <span class="card-icon" style="color: ${cardColor}"><i class="fas fa-user-graduate"></i></span>
-                <span class="student-id" style="color: ${cardColor}">${id}</span>
-                
+                <span class="student-id" style="color: ${cardColor}">${id}</span>                
             `;
-
             
             card.addEventListener('mouseenter', function() {
                 const color = this.dataset.color;
